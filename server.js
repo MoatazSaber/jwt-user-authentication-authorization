3f0190458cc89314636ca516b239b376ee31636a
MoatazSaber <moatazsaber96@gmail.com>
2022-05-22 16:31:55 +0200

    added cluster module for performance enhancements when spinning up server

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require("./app");
 const mongoose = require("mongoose");
+const cluster = require("cluster");
+const os = require("os");
+
+const cpuCount = os.cpus().length;
 
 //CONNECT TO MONGODB
 mongoose.connect(process.env.DB_CONNECTION_STRING, () =>
@@ -7,6 +11,15 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, () =>
 );
 
 //LISTEN
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`server is running on port ${process.env.PORT || 3000}!`)
-);
+
+if (cluster.isMaster) {
+  for (let i = 0; i < cpuCount; i++) {
+    cluster.fork();
+  }
+} else {
+  app.listen(process.env.PORT || 3000, () =>
+    console.log(
+      `🚀 server ${process.pid} is running on port ${process.env.PORT || 3000}!`
+    )
+  );
+}
